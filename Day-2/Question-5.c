a7567c432752b3dd2e8890fb41d8d71412c4f72f
rudrapratapgoyal2025-cloud <rudrapratap.goyal2025@glbajajgroup.org>
2026-06-10 22:11:40 +0530

    Day-2 uploaded

---
+++ Day-2/Question-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        digit = num % 10;   
+        sum += digit;     
+        num = num / 10;     
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
